83c7ea1592dee6904f98a4f7c797060d6d55cf53
Utkarsh Agarwal <35272322+utkarsh2019@users.noreply.github.com>
2018-11-13 22:32:12 -0500

    Changing 0 to -1 for classifiers

--- scripts/getData.py
+++ scripts/getData.py
@@ -4,15 +4,8 @@ import csv
 
 def getXY(filename):
     data = pd.read_csv(filename, sep=',',header=None)
-    # print(data)
     X = data[data.columns[0:29]].values[1:].astype(np.float)
     y = np.transpose([data[data.columns[30]].values[1:].astype(np.float)])
-    # print(X)
-    # print(y)
-    # print(type(X))
-    # print(X.shape)
-    # print(type(y))
-    # print(y.shape)
     return X,y
 
 if __name__ == "__main__":
