8f1dedde7f4e2059600cb9f223643009d92a3d91
Utkarsh Agarwal <35272322+utkarsh2019@users.noreply.github.com>
2018-11-16 19:44:45 -0500

    Changes for dual

--- scripts/kfoldcv.py
+++ scripts/kfoldcv.py
@@ -7,33 +7,66 @@ from sklearn import svm
 import kerdualsvm
 import kerpred
 
+# This Function may be obsolete now
 
-def measurePerformance(alpha, X, y, X1, y1):
-    """
-    This feature computes the score of a specific set, with the model learned
 
-    Input:
-        alpha: the model learned is denoted by alpha, and the X and y above
-        X: The data points from which the model is learnt
-        y: the labels for each data point from which the model is learnt
-        X1: The data points which are under consideration - for which the score
-            is to be computed
-        y1: The true labels of the data points X1 that are to be tested
+# def measurePerformance(alpha, X, y, X1, y1):
+#     """
+#     This feature computes the score of a specific set, with the model learned
 
-    Output:
-        A value between 0 and 1 inclusive, representing the score on this set
-    """
-    n, d = X1.shape
+#     Input:
+#         alpha: the model learned is denoted by alpha, and the X and y above
+#         X: The data points from which the model is learnt
+#         y: the labels for each data point from which the model is learnt
+#         X1: The data points which are under consideration - for which the score
+#             is to be computed
+#         y1: The true labels of the data points X1 that are to be tested
 
-    nCorrect = 0
-    for i in range(n):
-        x = X1[i]
-        x = x.reshape((d, 1))
-        prediction = kerpred.run(alpha, X, y, x)
-        if prediction == y1[i, 0]:
-            nCorrect += 1
+#     Output:
+#         A value between 0 and 1 inclusive, representing the score on this set
+#     """
+#     n, d = X1.shape
 
-    return nCorrect * 1.0 / n
+#     nCorrect = 0
+#     for i in range(n):
+#         x = X1[i]
+#         x = x.reshape((d, 1))
+#         prediction = kerpred.run(alpha, X, y, x)
+#         if prediction == y1[i, 0]:
+#             nCorrect += 1
+
+#     return nCorrect * 1.0 / n
+
+
+def processAll(X, y, type):
+
+    z = np.zeros(shape=(1, 1))
+
+    # for dual svm using sklearn
+    if (type == 1):
+        # clf = svm.SVC(kernel='linear',C=1,gamma=1)
+        clf = svm.SVC(kernel="rbf",gamma=0.0000001,C=100000)
+        clf.fit(X,y)
+        count = 0
+        lenY = len(y)
+        for j in range(lenY):
+            if y[j] == clf.predict([X[j]])[0]:
+                count += 1
+        z[0] = 100*float(lenY - count)/float(lenY)
+        print(z[0])
+
+    # for primal svm using sklearn
+    elif(type == 2):
+        clf = svm.LinearSVC(C=10,dual=False)
+        clf.fit(X,y)
+        count = 0
+        lenY = len(y)
+        for j in range(lenY):
+            if y[j] == clf.predict([X[j]])[0]:
+                count += 1
+        z[0] = 100*float(lenY - count)/float(lenY)
+        print(z[0])
+    return z
 
 # Input: number of folds k
 #        numpy matrix X of features, with n rows (samples), d columns
@@ -43,6 +76,13 @@ def measurePerformance(alpha, X, y, X1, y1):
 
 
 def run(k, X, y, type):
+    # check if k is less than or equal to 1
+    if (k < 1):
+        print("Please provide valid folds")
+        return []
+    elif (k == 1):
+        return processAll(X, y, type)
+
     n = X.shape[0]
 
     # this is a list containing all the k subsets. So it is of size k in the
@@ -85,25 +125,25 @@ def run(k, X, y, type):
 
         # for dual svm using sklearn
         if (type == 1):
-            clf = svm.SVC(kernel='linear',C=1,gamma=1)
+            clf = svm.SVC(kernel="rbf",gamma=0.0000001,C=100000)
             clf.fit(S,yForS_1)
             count = 0
             lenS = len(yForT)
             for j in range(lenS):
                 if yForT[j] == clf.predict([T[j]])[0]:
                     count += 1
-            z[i] = float(lenS - count)/float(lenS)
+            z[i] = 100*float(lenS - count)/float(lenS)
             print(z[i])
 
         # for primal svm using sklearn
         elif(type == 2):
-            clf = svm.LinearSVC(C=1,dual=False)
+            clf = svm.LinearSVC(C=10,dual=False)
             clf.fit(S,yForS_1)
             count = 0
             lenS = len(yForT)
             for j in range(lenS):
                 if yForT[j] == clf.predict([T[j]])[0]:
                     count += 1
-            z[i] = float(lenS - count)/float(lenS)
+            z[i] = 100*float(lenS - count)/float(lenS)
             print(z[i])
     return z
--- scripts/test.py
+++ scripts/test.py
@@ -3,36 +3,12 @@ import numpy as np
 
 from sklearn import svm
 
-import linpred as pred
-import linprimalsvm as own_svm
 import myopicfitting as mf
 import getData as gd
-import kfoldcv
+import kFoldCV
 
 F = 15
-FOLDS = 5
-
-def percentage(part, whole):
-  return 100 * float(part)/float(whole)
-
-def test_Pred(theta,X,y):
-    n = X.shape[1]
-    count = 0
-    for i in range(n):
-        if y[i,0] == pred.run(theta,np.transpose(np.array([X[i,:]]))):
-            count += 1
-    print("Error: " + str(percentage(n-count,n)) + "%")
-    return 1
-
-def test_Own_Primal_Svm(filename, S):
-    X,y = gd.getXY(filename)
-    n = X.shape[0]
-    X1 = X[:n//2,S[:,0]]
-    y1 = y[:n//2]
-    X2 = X[n//2:,S[:,0]]
-    y2 = y[n//2:]
-    theta = own_svm.run(X1,y1)
-    test_Pred(theta,X2,y2)
+FOLDS = 1
 
 def test_MF(filename):
     X,y = gd.getXY(filename)
@@ -52,34 +28,21 @@ def test_MF(filename):
     # plt.show()
     return S
 
-def test_Sklearn_Svm(filename):
-    X,y = gd.getXY(filename)
-    n = X.shape[0]
-    X1 = X[:n//2]
-    y1 = y[:n//2].reshape(n//2,)
-    X2 = X[n//2:]
-    y2 = y[n//2:].reshape(n//2,)
-    clf = svm.SVC(kernel='linear',C=1,gamma=1)
-    clf.fit(X1,y1)
-    count = 0
-    n1 = n//2
-    for i in range(n1):
-        if y2[i] == clf.predict([X2[i]])[0]:
-            count += 1
-    print("Error: " + str(percentage(n1-count,n1)) + "%")
-
-def test_K_Folds_CV(filename):
+def test_K_Folds_CV(filename,type):
     X,y = gd.getXY(filename)
     n = X.shape[0]
     y = y.reshape(n,)
-    z = kfoldcv.run(FOLDS, X, y, 1)
-    print("Error: ",(z.mean()*100))
+    if (type == "dual"):
+        z = kFoldCV.run(FOLDS, X, y, 1)
+    elif (type == "linear"):
+        z = kFoldCV.run(FOLDS, X, y, 2)
+    else:
+        print("Wrong SVM type")
+        return
+    print("Error: ",(z.mean()))
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 2):
-        print("Usage: python test.py <input_file>")
+    if (len(sys.argv) != 3):
+        print("Usage: python test.py <input_file> <svm_type>")
     else:
-        # S = test_MF(sys.argv[1])
-        # test_Own_Primal_Svm(sys.argv[1], S)
-        # test_Sklearn_Svm(sys.argv[1])
-        test_K_Folds_CV(sys.argv[1])
+        test_K_Folds_CV(sys.argv[1],sys.argv[2])
